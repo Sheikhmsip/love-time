300e3d5ea188c061718cd0d9fcc059f22d9a7f9d
Sheikhmsip <sheikhmsip@gmail.com>
2025-08-26 02:13:30 +0600

    add getMyFriends functionality

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,24 +1,38 @@
-import User from "../models/User";
+import User from "../models/User.js";
 
 // RecommendedUsers function
-export async function getRecommendedUsers(req, res){
-    try {
-        const currentUserId = req.user.id;
-        const currentUser = req.user;
+export async function getRecommendedUsers(req, res) {
+  try {
+    const currentUserId = req.user.id;
+    const currentUser = req.user;
 
-        const recommendedUsers = await User.find({
-            $and:[
-                {_id: {$ne: currentUserId}}, //exclude current user 
-                {$id: {$nin: currentUser.friends}}, //exclude current user's friends
-                {isOnboarded: true}
-            ]
-        })
-        res.status(200).json(recommendedUsers);
-    } catch (error) {
-        console.log("Error in getRecommendedUsers controller", error.message);
-        res.status(500).json({ message: "Internal Server Error" });
-    }
-};
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } }, //exclude current user
+        { $id: { $nin: currentUser.friends } }, //exclude current user's friends
+        { isOnboarded: true },
+      ],
+    });
+    res.status(200).json(recommendedUsers);
+  } catch (error) {
+    console.log("Error in getRecommendedUsers controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+}
 
+// My Friends funtion
+export async function getMyFriends(req, res) {
+  try {
+    const user = await User.findById(req.user.id)
+      .select("friends")
+      .populate(
+        "firends",
+        "fullName profilePic nativeLanguage learningLanguage"
+      );
 
-
+      res.status(200).json(user.friends);
+  } catch (error) {
+    console.log("Error in getMyFriends controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" }); 
+  }
+}
