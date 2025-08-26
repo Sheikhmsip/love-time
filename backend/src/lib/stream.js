6085a1055da8a3c55cc1c1fff045c0086edc755c
Sheikhmsip <sheikhmsip@gmail.com>
2025-08-26 18:01:10 +0600

    add Stream token generation and chat routes

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,23 +1,30 @@
-import {StreamChat} from "stream-chat";
+import { StreamChat } from "stream-chat";
 import "dotenv/config";
 
 const apiKey = process.env.STREAM_API_KEY;
 const apiSecret = process.env.STREAM_API_SECRET;
 
 if (!apiKey || !apiSecret) {
-    console.log("Stream API key or Cecret is missing");
+  console.log("Stream API key or Cecret is missing");
 }
-const streamClient = StreamChat.getInstance(apiKey, apiSecret); 
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
 
-
-export const upsertStreamUser = async(userData) => {
-    try {
-       await streamClient.upsertUsers([userData]);
-       return userData;
-    } catch (error) {
-        console.error("Error upserting Stream user", error);
-    }
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUsers([userData]);
+    return userData;
+  } catch (error) {
+    console.error("Error upserting Stream user", error);
+  }
 };
 
-// todo: do it later 
-export const generateStreamToken = (userId) => {};
+// ganerate stream token
+export const generateStreamToken = (userId) => {
+  try {
+    // ensure userId is a string
+    const userIdStr = userId.toString();
+    return streamClient.createToken(userIdStr);
+  } catch (error) {
+    console.error("Error generating Stream token", error);
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
-
+import chatRoutes from "./routes/chat.route.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 // app.get("/api/auth/signup", (req, res)=> {
 //     res.send("Signup Route");
@@ -28,6 +29,6 @@ app.use("/api/users", userRoutes);
 // });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-})
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
