053239ffa879a20428ce0543f62cb06b36d7548b
Sheikhmsip <sheikhmsip@gmail.com>
2025-08-27 21:58:40 +0600

    setup react route and add pages with router

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React from "react";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import NotificationsPage from "./pages/NotificationsPage.jsx";
+import CallPage from "./pages/CallPage.jsx";
+import OnboardingPage from "./pages/OnboardingPage.jsx";
+import ChatPage from "./pages/ChatPage.jsx";
 const App = () => {
   return (
-    <div className='bg-red-500 h-screen'>
-      App
+    <div className="h-screen" data-theme="night">
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/singup" element={<SignUpPage></SignUpPage>}></Route>
+        <Route path="/login" element={<LoginPage></LoginPage>}></Route>
+        <Route path="/notifications" element={<NotificationsPage></NotificationsPage>}></Route>
+        <Route path="/call" element={<CallPage></CallPage>}></Route>
+        <Route path="/chat" element={<ChatPage></ChatPage>}></Route>
+        <Route path="/onboarding" element={<OnboardingPage></OnboardingPage>}></Route>
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
