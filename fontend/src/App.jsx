8084d684f85a8b5c9a103f83890a730d8988cf21
Sheikhmsip <sheikhmsip@gmail.com>
2025-08-28 02:58:57 +0600

    add TanStack Query + axios instance, toast, and fixed backend cors configuration

--- fontend/src/App.jsx
+++ fontend/src/App.jsx
@@ -7,18 +7,39 @@ import NotificationsPage from "./pages/NotificationsPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "./lib/axios.js";
 const App = () => {
+  // Tanstake query
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/auth/me");
+
+      return res.data;
+    },
+    retry: false,
+  });
+  console.log(data);
   return (
     <div className="h-screen" data-theme="night">
       <Routes>
         <Route path="/" element={<HomePage></HomePage>}></Route>
         <Route path="/singup" element={<SignUpPage></SignUpPage>}></Route>
         <Route path="/login" element={<LoginPage></LoginPage>}></Route>
-        <Route path="/notifications" element={<NotificationsPage></NotificationsPage>}></Route>
+        <Route
+          path="/notifications"
+          element={<NotificationsPage></NotificationsPage>}></Route>
         <Route path="/call" element={<CallPage></CallPage>}></Route>
         <Route path="/chat" element={<ChatPage></ChatPage>}></Route>
-        <Route path="/onboarding" element={<OnboardingPage></OnboardingPage>}></Route>
+        <Route
+          path="/onboarding"
+          element={<OnboardingPage></OnboardingPage>}></Route>
       </Routes>
+
+      <Toaster></Toaster>
     </div>
   );
 };
