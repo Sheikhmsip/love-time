1b0247eb2f44fadb4d3d2996c59dd1af7e6457a4
Sheikhmsip <sheikhmsip@gmail.com>
2025-08-31 02:22:46 +0600

    design Navbar add ThemeSelector component and logout functionality

--- fontend/src/components/Navbar.jsx
+++ fontend/src/components/Navbar.jsx
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
+import useAuthUser from "../hooks/useAuthUser";
+import { Link, useLocation } from "react-router";
+import { BellIcon, LogOutIcon, ShipWheelIcon } from "lucide-react";
+import ThemeSelector from "./ThemeSelector";
+import useLogout from "../hooks/useLogout";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+  const { authUser } = useAuthUser();
+  const location = useLocation();
+  const isChatPage = location.pathname?.startsWith("/chat");
+
+// Use Logout hook 
+  const {logoutMutation} = useLogout();
+
+  return (
+    <nav className="bg-base-200 border-b border-base-300 sticky top-0 z-30 h-16 flex items-center">
+      <div className=" container mx-auto sm:px-6 lg:px-8">
+        <div className="flex items-center justify-end w-full">
+          {/* Logo-  Only in the chat page*/}
+          {isChatPage && (
+            <div className="pl-5">
+              <Link to="/" className="flex items-center gap-2.5">
+                <ShipWheelIcon className="size-9 text-primary" />
+                <span className="text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary  tracking-wider">
+                  Lovetime
+                </span>
+              </Link>
+            </div>
+          )}
+
+          <div className="flex items-center gap-3 sm:gap-4">
+            <Link to={"/notifications"}>
+              <button className="btn btn-ghost btn-circle">
+                <BellIcon className="size-6 text-base-content opacity-70"></BellIcon>
+              </button>
+            </Link>
+          </div>
+          {/* TODO: Will intragrate later */}
+          <ThemeSelector></ThemeSelector>
+          {/* Profile avatar */}
+          <div className="avatar">
+            <div className="w-9 rounded-full">
+              <img
+                src={authUser?.profilePic}
+                alt="User Avatar"
+                rel="noreferrer"></img>
+            </div>
+          </div>
+
+          {/* Logout button */}
+          <button className="btn btn-ghost btn-circle" onClick={logoutMutation}>
+            <LogOutIcon className="size-6 text-base-content opacity-70"></LogOutIcon>
+          </button>
         </div>
-    );
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ fontend/src/components/ThemeSelector.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ThemeSelector = () => {
+    return (
+        <div>
+            Theme Selector
+        </div>
+    );
+};
+
+export default ThemeSelector;
+ 
